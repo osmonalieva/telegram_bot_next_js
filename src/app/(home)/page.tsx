b4a5efaf5b377b0dba07ce3a0e12a1d5463b4d44
osmonalieva <nosmonalieva69@gmail.com>
2024-07-14 13:19:22 +0600

    first vercal

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactTelegram from '../components/ContactTelegram'
+
+const page = () => {
+  return (
+    <>
+      <ContactTelegram/>
+    </>
+  );
+};
+
+export default page;
